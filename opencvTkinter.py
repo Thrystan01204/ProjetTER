0aa31435648f97af0e7e7d0eab60b1853c25249a
LE LIDEC Tristan <tristanthomas42@gmail.com>
2021-02-10 17:44:02 +0400

    Tentative récupération image depuis la webcam et affichage dans une fenêtre Tkinter

--- opencvTkinter.py
+++ opencvTkinter.py
@@ -1,8 +1,21 @@
 from cv2 import cv2
 import numpy as np
+from tkinter import *
+from PIL import Image,ImageTk
 
+root = Tk()
+root.geometry("700x540")
+root.configure(bg="black")
+Label(root, text="Ma Cam", font=("times new roman", 30, "bold"), bg="black", fg="red").pack()
+f1 = LabelFrame(root, bg="red")
+f1.pack()
+L1 = Label(f1, bg="red")
+L1.pack()
 cap = cv2.VideoCapture(2)
 
-img1 = cap.read()[1]
-
-print(img1.shape)
+while True:
+    img = cap.read()[1]
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = ImageTk.PhotoImage(Image.fromarray(img))
+    L1['image'] = img
+    root.update()
