7f6c66670e6c33d20e0222ff8995cea212541cc4
SalvanTER <78851425+SalvanTER@users.noreply.github.com>
2021-02-10 18:13:12 +0400

    Add files via upload

--- detectionCorps.py
+++ detectionCorps.py
@@ -19,16 +19,19 @@ video_height= 720
 
 while True:
     ret, frame=cap.read()
-    frame = cv2.resize(frame, (video_width, video_height)) 
-    tickmark=cv2.getTickCount()
-    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face=face_cascade.detectMultiScale(gray, 1.3, 5)
-    for x, y, w, h in face:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    if cv2.waitKey(1)&0xFF==ord('q'):
-        break
-    fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
-    cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
-    cv2.imshow('video', frame)
+    if (counter % maximum_fps) == 0:
+        frame = cv2.resize(frame, (video_width, video_height)) 
+        tickmark=cv2.getTickCount()
+        gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        face=face_cascade.detectMultiScale(gray, 1.3, 5)
+        for x, y, w, h in face:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        if cv2.waitKey(1)&0xFF==ord('q'):
+            break
+        fps=cv2.getTickFrequency()/(cv2.getTickCount()-tickmark)
+        cv2.putText(frame, "FPS: {:05.2f}".format(fps), (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.imshow('video', frame)
+
+    counter += 1    
 cap.release()
 cv2.destroyAllWindows()
