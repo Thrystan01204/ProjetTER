28124dd1f372b69e434764d30a0226db89228acd
SalvanTER <78851425+SalvanTER@users.noreply.github.com>
2021-02-10 15:01:53 +0400

    Update detectionDeMouvement.py

--- detectionDeMouvement.py
+++ detectionDeMouvement.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 from cv2 import cv2
 
-cap=cv2.VideoCapture("http://192.168.43.1:4747/video")#Adresse IP de la camera, pour tester, installer une applis qui transforme le tel en camera IP
+cap=cv2.VideoCapture("http://192.168.43.1:4747/video")#Adresse IP de la camera, pour tester, installer une applis qui transforme le tel en camera IP droidCAM pour android
 
 kernel_blur=5
 seuil=15
