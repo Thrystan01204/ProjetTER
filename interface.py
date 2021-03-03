ba2283277be87a5d860559f31d4f0429731033c9
LE LIDEC Tristan <tristanthomas42@gmail.com>
2021-03-03 16:54:29 +0400

    Interface avec détection chute

--- interface.py
+++ interface.py
@@ -1,93 +1,124 @@
+from __future__ import print_function
 import tkinter as tk
 from PIL import Image
 from PIL import ImageTk
-from cv2 import cv2
+import cv2 as cv
+import argparse
+import numpy as np
 
-# root = tk.Tk()
 
-# canvas = tk.Canvas(root, height=800, width=1000)
-# canvas.pack()
+kernel_blur=5
+seuil=15
+surface=1000
+zoneX= 150
+zoneY= 150
+zoneW= 300
+zoneH= 300
+width= 1280
+height= 720
 
-# frame = tk.Frame(root, bg="blue")
-# frame.place(relheight=0.1, relwidth=1)
 
-# button = tk.Button(frame, text="Coucou", bg="white", fg="black", command=root.destroy)
-# button.pack(side=tk.TOP)
+video_width= 256*2
+video_height= 144*2
 
-# conteneur = tk.LabelFrame(frame, bg="black")
-# conteneur.pack()
+parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
+                                            OpenCV. You can process both videos and images.')
+parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='vtest.avi')
+#argument 'input' qui contient le chemin d'accès à une vidéo ou séquences d'images
 
-# video = tk.Label(conteneur, bg="black")
-# video.pack()
+parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
+#argument 'algo' qui définit le type de méthode pour la soustraction d'image (KNN, MOG2)
 
-# cap = cv2.VideoCapture(0)
+args = parser.parse_args()
 
-# while True:
-#     img = cap.read()[1]
-#     img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     img = ImageTk.PhotoImage(Image.fromarray(img1))
-#     video['image'] = img
-#     root.update()
+root = tk.Tk()
 
-class Interface(tk.Tk):
-    def __init__(self, parent=None, webcam=0, video=None):
-        tk.Tk.__init__(self, parent)
-        self.webcam = webcam
-        self.parent = parent
-        self.initialize()
-        self.video = video
+canvas = tk.Canvas(root)
+canvas.grid(column=0, row=0)
 
-    def initialize(self):
-        self.grid()
-        self.frame = tk.Frame(self.parent, bg="blue")
-        self.frame.grid(column=0, row=1, columnspan=2)
+frame = tk.Frame(root, bg="blue")
+frame.grid(column=0, row=0)
 
-        self.button = tk.Button(text=('Test button'))
-        self.button.grid(column=0, row=0)
+conteneur = tk.LabelFrame(frame, bg="black")
+conteneur.grid(column=0, row=0)
 
-        self.conteneur = tk.LabelFrame(self.frame, bg="black")
-        self.conteneur.grid(column=0, row=0)
+video = tk.Label(conteneur, bg="black")
+video.grid(column=0, row=0)
 
-        self.video = tk.Label(self.conteneur, bg="black")
-        self.video.grid(column=0, row=0)
+# [create]
+#Creation d'un fond de masque d'image (méthodes principales d'instanciaton)
 
-        self.cap = cv2.VideoCapture(self.webcam)
+if args.algo == 'MOG2':
+    backSub = cv.createBackgroundSubtractorMOG2()
+else:
+    backSub = cv.createBackgroundSubtractorKNN()
+## [create]
 
-    def update_video(self):
-        self.video.update_video()
-        self.video['image'] = self.video.img
-        self.update()
+## [capture]
+#On récupère le fichier vidéo et on le stoque dans une variable
+capture = cv.VideoCapture(0)
+if not capture.isOpened():
+    print('Unable to open: ' + args.input)
+    exit(0)
 
-    def get_webcam(self):
-        return self.webcam
-
-class video():
-    def __init__(self, video_width, video_height):
-        self.hog = cv2.HOGDescriptor()
-        self.hog.setSVMDetector( cv2.HOGDescriptor_getDefaultPeopleDetector() )
-        self.cap = cv2.VideoCapture(0)
-
-        self.video_width = video_width
-        self.video_height = video_height
-
-    def update_video(self):
-        _,self.frame=self.cap.read()
-        self.frame = cv2.resize(self.frame, (self.video_width, self.video_height)) 
-        self.found, self.w = self.hog.detectMultiScale(self.frame, winStride=(8,8), padding=(32,32), scale=1.05)
-        self.draw_detections(self.frame, self.found)
-        self.img1 = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
-        self.img = ImageTk.PhotoImage(Image.fromarray(self.img1))
-
-    def draw_detections(img, rects, thickness = 1):
-        for x, y, w, h in rects:
-            # the HOG detector returns slightly larger rectangles than the real objects.
-            # so we slightly shrink the rectangles to get a nicer output.
-            pad_w, pad_h = int(0.15*w), int(0.05*h)
-            cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 0, 255), thickness)
-
-vid = video(256*2, 144*2)
-app = Interface(video=vid)
-app.title('Test')
 
 while True:
-    app.update_video()
+    #lit chaque séquence d'image de la vidéo jusqu'à se qu'il y en ait plus(break)
+    ret, frame = capture.read()
+    frame= cv.resize(frame, (1280, 720));
+    if frame is None:
+        break
+    ## [apply]
+    #Met à jour le modele de masque de fond qu'on utilise (soustraction pixel)
+    fgMask = backSub.apply(frame)
+    
+    ## Détecte la personne qui fait une chute
+    mask=cv.GaussianBlur(fgMask, (kernel_blur, kernel_blur), 0)
+    mask=cv.threshold(mask, seuil, 255, cv.THRESH_BINARY)[1]
+    mask=cv.dilate(mask, np.ones((5, 5), np.uint8), iterations=3)
+    contours, nada=cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    frame_contour=frame.copy()
+    for c in contours:
+        cv.drawContours(frame_contour, [c], 0, (0, 255, 0), 5)
+        if cv.contourArea(c)<surface:
+            continue
+        x, y, w, h=cv.boundingRect(c)
+        if x>= zoneX and x <= zoneW and y>= zoneY and y <= zoneH :
+            cv.rectangle(frame_contour, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    ## [display_frame_number]
+    #création d' un rectangle qui va stocker l'image
+    cv.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    #ajout du numéro de la séquence d'image dans notre rectangle
+    cv.putText(frame, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+            cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    
+
+    ## [show]
+    #affichage graphique de l'image de fond
+    cv.putText(frame_contour, "[o|l]seuil: {:d}  [p|m]blur: {:d}  [i|k]surface: {:d}".format(seuil, kernel_blur, surface), (10, 30), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 2)
+    cv.rectangle(frame_contour, (zoneX, zoneY), (zoneX+zoneW, zoneY+zoneH), (255, 0, 0), 2)
+
+    #touche 'q' => fin de l'affichage
+    key=cv.waitKey(30)&0xFF
+    if key==ord('q'):
+        break
+    if key==ord('p'):
+        kernel_blur=min(43, kernel_blur+2)
+    if key==ord('m'):
+        kernel_blur=max(1, kernel_blur-2)
+    if key==ord('i'):
+        surface+=1000
+    if key==ord('k'):
+        surface=max(1000, surface-1000)
+    if key==ord('o'):
+        seuil=min(255, seuil+1)
+    if key==ord('l'):
+        seuil=max(1, seuil-1)
+    if key==ord('l'):
+        seuil=max(1, seuil-1)
+
+    img1 = cv.cvtColor(frame_contour, cv.COLOR_BGR2RGB)
+    img = ImageTk.PhotoImage(Image.fromarray(img1))
+    video['image'] = img
+    root.update()
